54391fea22569565e19b6aa927c6028fc908350d
German Renato Cobian <germancobian@hotmail.com>
2023-02-09 21:02:32 -0800

    Create TestPage provisional component to test logout button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ProtectedRoutes from './components/ProtectedRoutes';
 import useAuth from './hooks/useAuth';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import TestPage from './components/TestPage';
 
 function App() {
   const { authChecked, loggedIn } = useAuth();
@@ -15,7 +16,7 @@ function App() {
         <Route path="/login" element={<Login loggedIn={loggedIn} />} />
         <Route path="/signup" element={<Signup />} />
         <Route element={<ProtectedRoutes isAllowed={loggedIn} authChecked={authChecked} redirectPath="/login" />}>
-          <Route path="/" element={<h1>Test Page</h1>} />
+          <Route path="/" element={<TestPage />} />
         </Route>
       </Routes>
     </Router>
