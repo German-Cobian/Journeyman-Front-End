a67f9923da6dd26212891933499a896a6a18a279
German Renato Cobian <germancobian@hotmail.com>
2023-02-09 19:51:19 -0800

    Add CheckAuth method to auth actions + test connection to back end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { loginUser, checkAuth } from './redux/actions/auth';
 
 function App() {
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+  const handleClick = () => {
+    dispatch(loginUser({ username: 'AdminUser', email: 'admin@gmail.com', password: '123admin' }));
+  };
+
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, [dispatch]);
+
   return (
-    <div className="App">App component</div>
-  )
+    <Router>
+      <div className="App">
+        { auth.loggedIn ? 'logged in' : 'not logged in'}
+        <button type="button" onClick={handleClick}>get data</button>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
