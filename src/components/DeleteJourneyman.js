e3f87c83b0a434f998fd29c60ba4244ed235efd6
German Renato Cobian <germancobian@hotmail.com>
2023-02-14 11:12:53 -0800

    Correct overall format of the Delete Journeyman component

--- src/components/DeleteJourneyman.js
+++ src/components/DeleteJourneyman.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { displayJourneymen, deleteJourneyman } from '../redux/actions/journeymen';
 
 const DeleteJourneymanForm = () => {
-  const journeymen = useSelector((state) => state.journeymen);
   const dispatch = useDispatch();
+  const journeymen = useSelector((state) => state.journeymen);
+  const [slideIndex, setSlideIndex] = useState(0);
+  const lenghtJourneymen = journeymen.length;
+  const journeymanCard = useRef();
+  const size = 250;
 
   useEffect(() => {
     if (journeymen.length === 0) {
@@ -13,35 +16,75 @@ const DeleteJourneymanForm = () => {
     }
   }, []);
 
+    const nextSlide = () => {
+      if (slideIndex === lenghtJourneymen - 1) return;
+      const current = slideIndex + 1;
+      journeymanCard.current.style.transform = `translateX(${-size * current}px)`;
+      setSlideIndex(current);
+    };
+  
+    const prevSlide = () => {
+      if (slideIndex === 0) return;
+      const current = slideIndex - 1;
+      journeymanCard.current.style.transform = `translateX(${-size * current}px)`;
+      setSlideIndex(current);
+    };
+
+    
+
   return (
     <main className="">
-      <div className="d-flex flex-column align-items-center mx-5">
-        {journeymen.map((journeyman) => (
-          <div key={journeyman.id} className="card my-5 mx-5">
-            <div className="my-3 mx-5">
-              <img src={journeyman.image_url} alt="journeyman-img" width="150" height="150" />
-            </div>
-            <div className="border border-light">
-              <div className="my-5 mx-5">
-                <p className="">{journeyman.name}</p>
-                <p className="">{journeyman.skill}</p>
-                <p className="">{journeyman.country}</p>
-                <p className="">{journeyman.city}</p>
-                <p className="">{journeyman.price}</p>
+      <div className="">
+        <div ref={journeymanCard} className="d-flex flex-row">
+          {journeymen.map((journeyman) => (
+            <div key={journeyman.id} className="d-flex flex-column align-items-center border border-light my-3 mx-3">
+              <div className="my-3 mx-5">
+                <img src={journeyman.image_url} alt="journeyman-img" width="150" height="150" />
               </div>
-              <div className="mx-5 my-5">
-                <button
-                  type="button"
-                  className="btn btn-danger btn-lg"
-                  onClick={() => dispatch(deleteJourneyman(journeyman.id))}
-                >
-                  Delete
-                </button>
+              <div className="div-flex flex-column my-3 mx-5">
+                <p className="">
+                  <span className="font-weight-bold">Journeyman Name: </span>
+                  {journeyman.name}
+                </p>
+                <p className="">
+                  <span className="font-weight-bold">Journeyman Skill: </span>
+                  {journeyman.skill}
+                </p>
+                <p className="">
+                  <span className="font-weight-bold">Country: </span>
+                  {journeyman.country}
+                </p>
+                <p className="">
+                  <span className="font-weight-bold">City: </span>
+                  {journeyman.city}
+                </p>
+                <p className="">
+                  <span className="font-weight-bold">Price per Day: </span>
+                  $
+                  {journeyman.price}
+                </p>
+                <div className="my-3">
+                  <button
+                    type="button"
+                    className="btn btn-danger btn-lg"
+                    onClick={() => dispatch(deleteJourneyman(journeyman.id))}
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
+      <div className="d-flex flex-row justify-content-between">
+        <button className="btn btn-primary btn-lg" type="button" onClick={nextSlide}>
+          Right
+        </button>
+        <button className="btn btn-primary btn-lg" type="button" onClick={prevSlide}>
+          Left
+        </button>
+      </div>  
     </main>
   );
 };
